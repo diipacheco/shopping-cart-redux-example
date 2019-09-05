9874bc51ac0320c2902a4caebf22e188875891d7
diipacheco <edilsonpacheco6@gmail.com>
2019-09-05 10:54:13 -0300

    Refactoring of default api url

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/diipacheco/shopping-cart-json-server',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
