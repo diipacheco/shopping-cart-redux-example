bead10a8d2ee85d11116f865606135ef636e8e58
diipacheco <edilsonpacheco6@gmail.com>
2019-09-05 10:40:48 -0300

    Changes the default url for HTTP client

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://my-json-server.typicode.com/diipacheco/shopping-cart-json-server',
 });
 
 export default api;
